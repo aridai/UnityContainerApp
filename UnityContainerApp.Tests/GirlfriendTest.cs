f2178afe57d933a165d05014e4b2da8d2ca4ba93
aridai <aridai.net@outlook.jp>
2019-05-05 08:15:24 +0900

    テストを追加した。

---
+++ UnityContainerApp.Tests/GirlfriendTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unity;
+using UnityContainerApp.Models;
+
+namespace UnityContainerApp.Tests
+{
+    [TestClass]
+    public sealed class GirlfriendTest
+    {
+        [TestMethod]
+        public void テスト用彼女が注入されていることを確認するテスト()
+        {
+            var container = new UnityContainer();
+            container.RegisterType<Girlfriend, Alice>();
+
+            var couple = container.Resolve<Couple>();
+            Assert.AreEqual("aridaiとアリスはカップルです。", couple.ToString());
+        }
+    }
+}
---
+++ UnityContainerApp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnityContainerApp.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnityContainerApp.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cda470f2-b020-45f4-8207-bc401630a430")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
