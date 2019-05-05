b89e0c3ce928068264614cd2adfffc437305429d
aridai <aridai.net@outlook.jp>
2019-05-05 15:15:00 +0900

    Debug/ReleaseビルドによってDIの設定を変えるようにした。

--- UnityContainerApp/App.xaml.cs
+++ UnityContainerApp/App.xaml.cs
@@ -14,7 +14,23 @@ namespace UnityContainerApp
         protected override void OnStartup(StartupEventArgs e)
         {
             //  ここでDIコンテナの設定をする。
+            //  Debug/ReleaseでDIの設定を切り替えている。
 
+#if DEBUG
+            this.RegisterDependenciesForDebug();
+#else
+            this.RegisterDependenciesForRelease();
+#endif
+
+            //  AutoWireViewModelを有効にしたViewに流し込むViewModelを設定する。
+            //  ViewModelLocatorはViewの名前の末尾に「ViewModel」が付いているクラスの、
+            //  引数なしコンストラクタで生成しようとするのだと思う。
+            ViewModelLocationProvider.SetDefaultViewModelFactory(type => this.container.Resolve(type));
+        }
+
+        //  リリースビルド用に依存関係の登録を行う。
+        private void RegisterDependenciesForRelease()
+        {
             //  インターフェースと具象型を紐付ける。
             //  これは引数なしコンストラクタを持つため、型での紐付けで良い。
             this.container.RegisterType<Girlfriend, Marisa>();
@@ -33,11 +49,12 @@ namespace UnityContainerApp
 
             //  これも、具象型かつコンストラクタの全ての引数が解決できるため、別に書かなくてもいい。
             //  this.container.RegisterInstance<MainWindowViewModel>(new MainWindowViewModel(this.container.Resolve<Couple>()));
+        }
 
-            //  AutoWireViewModelを有効にしたViewに流し込むViewModelを設定する。
-            //  ViewModelLocatorはViewの名前の末尾に「ViewModel」が付いているクラスの、
-            //  引数なしコンストラクタで生成しようとするのだと思う。
-            ViewModelLocationProvider.SetDefaultViewModelFactory(type => this.container.Resolve(type));
+        //  デバッグビルド用に依存関係の登録を行う。
+        private void RegisterDependenciesForDebug()
+        {
+            this.container.RegisterType<Girlfriend, Alice>();
         }
     }
 }
